cbac328625be71d2a2467f884a39c52655543538
Charles Marchant <chuckm248@gmail.com>
2024-02-26 15:10:46 -0700

    refactor pricing project and fix errors in sass template

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+// import { resolve } from "path";
+// import { defineConfig } from "vite";
 
-export default defineConfig({
-  root: resolve(__dirname, "src/"),
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        home: resolve(__dirname, "src/index.html"),
-        about: resolve(__dirname, "src/about/index.html"),
-        contact: resolve(__dirname, "src/contact/index.html"),
-      },
-    },
-  },
-});
+// export default defineConfig({
+//   root: resolve(__dirname, "src/"),
+//   build: {
+//     outDir: "../dist",
+//     emptyOutDir: true,
+//     rollupOptions: {
+//       input: {
+//         home: resolve(__dirname, "src/index.html"),
+//         about: resolve(__dirname, "src/about/index.html"),
+//         contact: resolve(__dirname, "src/contact/index.html"),
+//       },
+//     },
+//   },
+// });
